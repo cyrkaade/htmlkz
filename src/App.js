52c6578f1c6fe98516f3362e240b6ee0519cc924
John Doe <johndoe@email.com>
2023-06-29 16:27:50 +0600

    3

--- src/App.js
+++ src/App.js
@@ -1,65 +1,42 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'; // Import the CSS file for styling
+import jsonData from './texts/text1.json';
 
-function App() {
-  const handleCollapsibleToggle = () => {
+const App = () => {
+  const [buttonText, setButtonText] = useState('Button');
+  const [htmlCode, setHtmlCode] = useState('');
 
-  var coll = document.getElementsByClassName("collapsible");
-  var i;
-
-  for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var content = this.nextElementSibling;
-      if (content.style.display === "block") {
-      content.style.display = "none";
-      } else {
-      content.style.display = "block";
-      }
-  });
-  }
+  const handleHtmlCodeChange = (event) => {
+    setHtmlCode(event.target.value);
   };
+
+  const handleButtonClick = () => {
+    setButtonText(htmlCode === jsonData.code ? 'Correct' : 'Incorrect');
+  };
+
   return (
-    <div>
-    <h1>Courses</h1>
-<h1>Basic HTML and HTML5</h1>
-<p>HTML is a markup language that uses a special syntax or notation to describe the structure of a webpage to the browser. HTML elements usually have opening and closing tags that surround and give meaning to content. For example, different elements can describe text as a heading, paragraph, or list item.
+    <div className="container">
+      <div className="section">
+        <h2>{jsonData.title}</h2>
+        <p>{jsonData.text}</p>
+        <button onClick={handleButtonClick}>{buttonText}</button>
+      </div>
 
-In this course, you'll build a cat photo app to learn some of the most common HTML elements — the building blocks of any webpage.</p>
-<button type="button" class="collapsible" onClick={handleCollapsibleToggle}>Open Collapsible</button>
-<div class="content">
-    <ul>
-        <li><a href="">Say Hello to HTML Elements</a></li>
-        <li><a href="">Headline with the h2 Element</a></li>
-        <li><a href="">Inform with the Paragraph Element</a></li>
-        <li><a href="">Fill in the Blank with Placeholder Text</a></li>
-        <li><a href="">Uncomment HTML</a></li>
-        <li><a href="">Say Hello to HTML Elements</a></li>
-        <li><a href="">Headline with the h2 Element</a></li>
-        <li><a href="">Inform with the Paragraph Element</a></li>
-        <li><a href="">Fill in the Blank with Placeholder Text</a></li>
-        <li><a href="">Uncomment HTML</a></li>
-        <li><a href="">Say Hello to HTML Elements</a></li>
-        <li><a href="">Headline with the h2 Element</a></li>
-        <li><a href="">Inform with the Paragraph Element</a></li>
-        <li><a href="">Fill in the Blank with Placeholder Text</a></li>
-        <li><a href="">Uncomment HTML</a></li>
-        <li><a href="">Say Hello to HTML Elements</a></li>
-        <li><a href="">Headline with the h2 Element</a></li>
-        <li><a href="">Inform with the Paragraph Element</a></li>
-        <li><a href="">Fill in the Blank with Placeholder Text</a></li>
-        <li><a href="">Uncomment HTML</a></li>
-        <li><a href="">Say Hello to HTML Elements</a></li>
-        <li><a href="">Headline with the h2 Element</a></li>
-        <li><a href="">Inform with the Paragraph Element</a></li>
-        <li><a href="">Fill in the Blank with Placeholder Text</a></li>
-        <li><a href="">Uncomment HTML</a></li>
-    </ul>
-</div>
+      <div className="section">
+        <h2>HTML Code Section</h2>
+        <textarea
+          value={htmlCode}
+          onChange={handleHtmlCodeChange}
+          placeholder="Insert your HTML code here"
+        />
+      </div>
 
+      <div className="section">
+        <h2>HTML Code Result</h2>
+        <div dangerouslySetInnerHTML={{ __html: htmlCode }} />
+      </div>
     </div>
-
   );
-}
+};
 
 export default App;
