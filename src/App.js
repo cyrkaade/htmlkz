47fd8d306a7b39ae7882f1b01fee9ad70e19452f
John Doe <johndoe@email.com>
2023-06-29 15:22:50 +0600

    2

--- src/App.js
+++ src/App.js
@@ -1,33 +1,64 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
 function App() {
-  const [htmlCode, setHtmlCode] = useState('');
+  const handleCollapsibleToggle = () => {
 
-  const handleHtmlCodeChange = (event) => {
-    setHtmlCode(event.target.value);
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+      content.style.display = "none";
+      } else {
+      content.style.display = "block";
+      }
+  });
+  }
   };
   return (
-    <div className="container">
-      <div className="section">
-        <h2>Text Section</h2>
-        <p>Insert your text here.</p>
-      </div>
+    <div>
+    <h1>Courses</h1>
+<h1>Basic HTML and HTML5</h1>
+<p>HTML is a markup language that uses a special syntax or notation to describe the structure of a webpage to the browser. HTML elements usually have opening and closing tags that surround and give meaning to content. For example, different elements can describe text as a heading, paragraph, or list item.
 
-      <div className="section">
-        <h2>HTML Code Section</h2>
-        <textarea
-          value={htmlCode}
-          onChange={handleHtmlCodeChange}
-          placeholder="Insert your HTML code here"
-        />
-      </div>
+In this course, you'll build a cat photo app to learn some of the most common HTML elements — the building blocks of any webpage.</p>
+<button type="button" class="collapsible" onClick={handleCollapsibleToggle}>Open Collapsible</button>
+<div class="content">
+    <ul>
+        <li><a href="">Say Hello to HTML Elements</a></li>
+        <li><a href="">Headline with the h2 Element</a></li>
+        <li><a href="">Inform with the Paragraph Element</a></li>
+        <li><a href="">Fill in the Blank with Placeholder Text</a></li>
+        <li><a href="">Uncomment HTML</a></li>
+        <li><a href="">Say Hello to HTML Elements</a></li>
+        <li><a href="">Headline with the h2 Element</a></li>
+        <li><a href="">Inform with the Paragraph Element</a></li>
+        <li><a href="">Fill in the Blank with Placeholder Text</a></li>
+        <li><a href="">Uncomment HTML</a></li>
+        <li><a href="">Say Hello to HTML Elements</a></li>
+        <li><a href="">Headline with the h2 Element</a></li>
+        <li><a href="">Inform with the Paragraph Element</a></li>
+        <li><a href="">Fill in the Blank with Placeholder Text</a></li>
+        <li><a href="">Uncomment HTML</a></li>
+        <li><a href="">Say Hello to HTML Elements</a></li>
+        <li><a href="">Headline with the h2 Element</a></li>
+        <li><a href="">Inform with the Paragraph Element</a></li>
+        <li><a href="">Fill in the Blank with Placeholder Text</a></li>
+        <li><a href="">Uncomment HTML</a></li>
+        <li><a href="">Say Hello to HTML Elements</a></li>
+        <li><a href="">Headline with the h2 Element</a></li>
+        <li><a href="">Inform with the Paragraph Element</a></li>
+        <li><a href="">Fill in the Blank with Placeholder Text</a></li>
+        <li><a href="">Uncomment HTML</a></li>
+    </ul>
+</div>
 
-      <div className="section">
-        <h2>HTML Code Result</h2>
-        <div dangerouslySetInnerHTML={{ __html: htmlCode }} />
-      </div>
     </div>
+
   );
 }
 
